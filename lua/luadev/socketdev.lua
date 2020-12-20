d1e9e513a93f2fe54e71dc5bbf2d0dd3ad9dd55d
deinferno <14363193+deinferno@users.noreply.github.com>
2020-12-20 12:54:05 +0000

    Fixed socketdev

--- lua/luadev/socketdev.lua
+++ lua/luadev/socketdev.lua
@@ -7,10 +7,10 @@ local function requireExists(moduleName)
 		or (system.IsOSX() and "osx"),
 		"couldn't determine system type?"
 	)
-	local dllFiles = file.Find(string.format("lua/bin/gmcl_%s_%s.dll", moduleName, osSuffix), "GAME")
+	local dllFiles = file.Find(string.format("lua/bin/gmcl_%s*_%s.dll", moduleName, osSuffix), "GAME")
 	local luaFileExists = file.Exists(string.format("includes/modules/%s.lua", moduleName), "LUA") or file.Exists(string.format("includes/modules/%s.lua", moduleName), "LCL")
 
-	return #dllFiles > 0 or luaFileExists
+	return #dllFiles > 0 and luaFileExists
 end
 
 local function luadevPrint(...)
